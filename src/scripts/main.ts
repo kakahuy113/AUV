30e95392b71f567cf43032b5dc1c628c3f972002
SinKy0225 <91duatndu1516@gmail.com>
2020-12-19 11:48:58 +0700

    about-auv

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,13 +2,7 @@ import { getSVGs, Loading } from "./utilities/util";
 import Axios from "axios";
 import Tab from './libraries/Tab';
 declare var Swiper:any;
-
-document.addEventListener("DOMContentLoaded", async () => {
-	getSVGs(".svg");
-	Loading();
-	const example = new Tab(".executive-committee .tab-container");
-
-});
+declare var $:any;
 const auv = new Swiper('.auv-section .swiper-container', {
 	slidesPerView: 5,
 	spaceBetween: 10,
@@ -34,3 +28,45 @@ const auv = new Swiper('.auv-section .swiper-container', {
 	  }
 	}
 })
+
+const ImagePopupabout = () => {
+
+	$(".lib__page__img .item-gallery").on("click" , function() {
+		const slides = $(this).find(".d-none")[0].innerHTML
+		$(".gallery-top .swiper-wrapper").html(`${slides}`)
+		$(".gallery-thumbs .swiper-wrapper").html(`${slides}`)
+		$.fancybox.open({
+			src: "#popup-img",
+			type: "inline",
+			opts : {
+				afterShow : function( ) {
+					var galleryThumbs = new Swiper('.gallery-thumbs', {
+						spaceBetween: 10,
+						slidesPerView: 3,
+					
+					  });
+					  var galleryTop = new Swiper('.gallery-top', {
+						spaceBetween: 10,
+						navigation: {
+						  nextEl: '.swiper-button-next',
+						  prevEl: '.swiper-button-prev',
+						},
+						thumbs: {
+						  swiper: galleryThumbs
+						}
+					  });
+				}
+			}
+		})
+		
+	})
+
+}
+document.addEventListener("DOMContentLoaded", async () => {
+	getSVGs(".svg");
+	Loading();
+	ImagePopupabout();
+	const example = new Tab(".executive-committee .tab-container");
+
+});
+
