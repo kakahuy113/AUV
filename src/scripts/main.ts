113b3c210edeea490ac6c394e922d59aeb5ecd06
kakahuy113 <kakahuy113@gmail.com>
2021-02-04 11:08:20 +0700

    form

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -117,30 +117,31 @@ const popupIndex = () => {
 		$.fancybox.open({
 			src: "#popup-page",
 			type: "inline",
-			opts: {
-				beforeShow : function() {
-					const condition = document.querySelectorAll(".index-popup .swiper-slide")
-					if(condition.length > 0) {
-						var swiper = new Swiper(".index-popup .swiper-container", {
-							slidesPerView: 1
-							// autoplay: {
-							// 	delay: 2500,
-							// },
-						});
-					}
-					
-				}
-			}
+			// opts: {
+			// 	afterShow : function() {
+			// 		const condition = document.querySelectorAll(".index-popup .swiper-slide")
+			// 		if(condition.length  <= 3) {
+			// 			const swiper = new Swiper(".index-popup .swiper-container", {
+			// 				slidesPerView: condition.length,
+			// 				spaceBetween: 10
+			// 				// autoplay: {
+			// 				// 	delay: 2500,
+			// 				// },
+			// 			});
+			// 		} else {
+			// 			const swiper = new Swiper(".index-popup .swiper-container", {
+			// 				slidesPerView: 3,
+			// 				spaceBetween: 10
+			// 				// autoplay: {
+			// 				// 	delay: 2500,
+			// 				// },
+			// 			});
+			// 		}
+			// 	}
+			// }
 		})
 	}
 }
-//popup-page
-// $(document).ready(function () {
-//     $("#popup-page").fancybox({
-//         'overlayShow': true
-//     }).trigger('click');
-// });
-
 
 //popup image Gallery
 const ImagePopugalley = () => {
@@ -545,85 +546,97 @@ const LogicFormApply = () => {
 
 		const ajaxApplyForm = (e:any) => {
 			e.preventDefault();
+
 			const formData = new FormData();
 			const url = document.querySelector(".tab-footer button").getAttribute("data-url")
+			//recaptcha
+			const recaptcha = $(".g-recaptcha")
+			formData.append(recaptcha.attr("name") , recaptcha.val());
 			//Tab 1
 			const tab = document.querySelectorAll(".apply-form .content")
-			tab[0].querySelectorAll("input[type=text]").forEach((el:HTMLInputElement) => {
 
-				const name = el.getAttribute("name");
-				const val = el.value;
-				formData.append(name , val);
-			})
-			tab[0].querySelectorAll("select").forEach( (el: HTMLSelectElement) => {
-				const name = el.getAttribute("name");
-				const val = el.value;
-				formData.append(name , val);
-			})
-			tab[0].querySelectorAll(".row-check").forEach((el:HTMLElement) => {
-				if(el.querySelector("input[type=checkbox]")) {
-					const name = el.querySelector("input").getAttribute("name")
-					const array = new Array();
-					el.querySelectorAll("input[type=checkbox]:checked").forEach((el:HTMLInputElement) => {
-						array.push(el.value);
-					})
-					if(array.length < 1) {
-						formData.append(name , null);
-					} else {
-						formData.append(name , JSON.stringify(array));
-					}
-				}
-				if(el.querySelector("input[type=radio]")) {
-					const name = el.querySelector("input").getAttribute("name")
-					const value = el.querySelector<HTMLInputElement>("input[type=radio]").value;
-					formData.append(name , value);
-				}
-			})
+			if(tab[2].querySelectorAll("input[type=checkbox]:checked").length > 1) {
 
-			// Tab 2
-			tab[1].querySelectorAll('input:not([type="file"])').forEach((el:HTMLInputElement) => {
+				tab[0].querySelectorAll("input[type=text]").forEach((el:HTMLInputElement) => {
+
 					const name = el.getAttribute("name");
 					const val = el.value;
 					formData.append(name , val);
-			})
-			tab[1].querySelectorAll('input[type="file"]').forEach((el:HTMLInputElement) => {
-				const name = el.getAttribute("name");
-				const file = el.files[0]
-				formData.append(name , file);
-			})
-
-			tab[2].querySelectorAll("input[hidden]").forEach((el:HTMLInputElement) => {
-				const name = el.getAttribute("name");
-				const val = el.value;
-				formData.append(name , val);
-			})
-
-			// if($(".admission-how-to-apply__wrapper form").valid() === true) {
-				$.ajax({
-					url : url,
-					data: formData,
-					type: "POST",
-					processData: false,
-					contentType: false,
-					beforeSend: function() {
-						e.target.setAttribute("disabled" , "disabled")
-					},
-					success: function(res:any) {
-						e.target.removeAttribute("disabled" , "disabled")
-						if(res.Code == 200) {
-							alert(res);
-							window.location.reload();
-						} else {
-							alert(res);
-						}
-					},
-					// error: function(res:any) {
-					// 	e.target.removeAttribute("disabled" , "disabled")
-					// 	alert(res);
-					// 	window.location.reload();
-					// }
 				})
-			// }
+				tab[0].querySelectorAll("select").forEach( (el: HTMLSelectElement) => {
+					const name = el.getAttribute("name");
+					const val = el.value;
+					formData.append(name , val);
+				})
+				tab[0].querySelectorAll(".row-check").forEach((el:HTMLElement) => {
+					if(el.querySelector("input[type=checkbox]")) {
+						const name = el.querySelector("input").getAttribute("name")
+						const array = new Array();
+						el.querySelectorAll("input[type=checkbox]:checked").forEach((el:HTMLInputElement) => {
+							array.push(el.value);
+						})
+						if(array.length < 1) {
+							formData.append(name , null);
+						} else {
+							formData.append(name , JSON.stringify(array));
+						}
+					}
+					if(el.querySelector("input[type=radio]")) {
+						const name = el.querySelector("input").getAttribute("name")
+						const value = el.querySelector<HTMLInputElement>("input[type=radio]").value;
+						formData.append(name , value);
+					}
+				})
+	
+				// Tab 2
+				tab[1].querySelectorAll('input:not([type="file"])').forEach((el:HTMLInputElement) => {
+						const name = el.getAttribute("name");
+						const val = el.value;
+						formData.append(name , val);
+				})
+				tab[1].querySelectorAll('input[type="file"]').forEach((el:HTMLInputElement) => {
+					const name = el.getAttribute("name");
+					const file = el.files[0]
+					formData.append(name , file);
+				})
+
+				tab[2].querySelectorAll("input[hidden]").forEach((el:HTMLInputElement) => {
+					const date = tab[2].querySelector<HTMLInputElement>(".date-picker").value;
+					const time = tab[2].querySelector<HTMLInputElement>(".time-picker").value;
+					const name = el.getAttribute("name");
+					const val = `${date}-${time}`;
+					formData.append(name , val);
+				})
+	
+				if($(".admission-how-to-apply__wrapper form").valid() === true) {
+					$.ajax({
+						url : url,
+						data: formData,
+						type: "POST",
+						processData: false,
+						contentType: false,
+						beforeSend: function() {
+							e.target.setAttribute("disabled" , "disabled")
+						},
+						success: function(res:any) {
+							e.target.removeAttribute("disabled" , "disabled")
+							if(res.Code == 200) {
+								alert(res);
+								window.location.reload();
+							} else {
+								alert(res);
+							}
+						},
+						// error: function(res:any) {
+						// 	e.target.removeAttribute("disabled" , "disabled")
+						// 	alert(res);
+						// 	window.location.reload();
+						// }
+					})
+				}
+			} else {
+				alert(`${e.target.getAttribute("data-warning")}`)
+			}
 		}
 
 		$(document).on("click" , ".apply-tab .tab" , function() {
@@ -877,6 +890,7 @@ window.onload = () => {
 		const button: HTMLButtonElement = document.querySelector(".fake-button-recaptcha");
 		button.click();
 	}
+	
 }
 
 document.addEventListener("scroll" , async () => {
